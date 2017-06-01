c470fd7290b56abb02d12fe91d90044b9f47a30c
morm <tmozhar@gmail.com>
2017-06-01 17:57:19 +0300

    log through util.inspect

--- routes/aproved.js
+++ routes/aproved.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var util = require('util');
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-	console.log("Aproved" + JSON.stringify(req));
+	console.log("Aproved" + util.inspect(req));
 	res.redirect('https://192.168.0.142/endp/verifyPassword');
 });
 
--- routes/canceled.js
+++ routes/canceled.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var util = require('util');
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  	console.log("Canceled" + JSON.stringify(req));
-  	res.redirect('https://192.168.0.142/endp/verifyPassword');
+	console.log("Canceled" + util.inspect(req));
+	res.redirect('https://192.168.0.142/endp/verifyPassword');
 });
 
 module.exports = router;
