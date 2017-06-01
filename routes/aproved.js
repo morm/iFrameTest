3388ea21ac842ea6d13fd3ba94e2f66313d2b4a7
morm <tmozhar@gmail.com>
2017-06-01 15:49:07 +0300

    aproved and canceled are POST

--- routes/aproved.js
+++ routes/aproved.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('aproved', { title: 'Approved', params: JSON.stringify(req.query)});
+router.post('/', function(req, res, next) {
+	console.log("Aproved" + JSON.stringify(req.query));
 });
 
 module.exports = router;
