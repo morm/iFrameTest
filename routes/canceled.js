a21fc2e7c030357a17d2b231240bcfa57bc32813
morm <tmozhar@gmail.com>
2017-06-01 17:39:47 +0300

    redirect added

--- routes/canceled.js
+++ routes/canceled.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.post('/', function(req, res, next) {
   	console.log("Canceled" + JSON.stringify(req.query));
+  	res.redirect('https://192.168.0.142/endp/verifyPassword');
 });
 
 module.exports = router;
