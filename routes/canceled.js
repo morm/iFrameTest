786721a943382e3e622e18bc6cc2d2ac48c31fbb
morm <tmozhar@gmail.com>
2017-06-01 17:44:25 +0300

    log req

--- routes/canceled.js
+++ routes/canceled.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  	console.log("Canceled" + JSON.stringify(req.query));
+  	console.log("Canceled" + JSON.stringify(req));
   	res.redirect('https://192.168.0.142/endp/verifyPassword');
 });
 
