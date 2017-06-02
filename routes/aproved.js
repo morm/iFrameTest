3ed9dee78249ff1f6c28ec7b1ea76898d7230fc0
morm <tmozhar@gmail.com>
2017-06-02 17:51:44 +0300

    req.body logging

--- routes/aproved.js
+++ routes/aproved.js
@@ -4,7 +4,7 @@ var util = require('util');
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-	console.log("Aproved" + util.inspect(req));
+	console.log("Aproved" + util.inspect(req.body));
 	res.redirect('https://192.168.0.142/endp/verifyPassword');
 });
 
