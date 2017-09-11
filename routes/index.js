c3d518c6b7291df01e92f6938ea4d9ee0f1f1ef4
mor_t <mor@front.ru>
2017-09-11 17:45:41 +0300

    fixes #1889

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if(req.query.lng==='ru')
+    res.render('index', { title: 'Express' , d4w_url: 'https://d4w-portal.ru/d4w/org-105/fsearch?practiceID=240'});
+  else
+    res.render('index', { title: 'Express' , d4w_url: 'https://obtest18.centaurportal.com/d4w/org-16/fsearch'});
 });
 
 module.exports = router;
