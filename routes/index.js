d592e7b0e643e2db706567d033e48dacc1a80765
mor_t <mor@front.ru>
2018-12-20 17:33:25 +0300

    popup view added

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,11 @@ router.get('/', function(req, res, next) {
   else
     res.render('index', { title: 'Express' , d4w_url: 'https://obtest18.centaurportal.com/d4w/org-16/fsearch'});
 });
+router.get('/popup', function(req, res, next) {
+  if(req.query.lng==='ru')
+    res.render('popup', { title: 'Express' , d4w_url: 'https://d4w-portal.ru/d4w/org-105/fsearch?practiceID=240'});
+  else
+    res.render('popup', { title: 'Express' , d4w_url: 'https://obtest18.centaurportal.com/d4w/org-16/fsearch'});
+});
 
 module.exports = router;
