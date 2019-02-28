73bad579ffc080db2e53d48305e17259d09f061b
Kirill Markidonov <markidonov@mail.ru>
2019-02-28 18:28:48 +0300

    Change titles to see the difference

--- routes/index.js
+++ routes/index.js
@@ -22,16 +22,16 @@ router.get('/prod', function(req, res, next) {
 
 router.get('/org-16', function(req, res, next) {
   if(req.query.lng==='ru')
-    res.render('popup', { title: 'Express' , d4w_url: 'https://d4w-portal.ru/d4w/org-105/fsearch?practiceID=240'});
+    res.render('popup', { title: 'org-16 RC' , d4w_url: 'https://d4w-portal.ru/d4w/org-105/fsearch?practiceID=240'});
   else
-    res.render('popup', { title: 'Express' , d4w_url: 'https://obtest18.centaurportal.com/d4w/org-16/fsearch'});
+    res.render('popup', { title: 'org-16 RC' , d4w_url: 'https://obtest18.centaurportal.com/d4w/org-16/fsearch'});
 });
 
 router.get('/ndc', function(req, res, next) {
   if(req.query.lng==='ru')
-    res.render('popup', { title: 'Express' , d4w_url: 'https://d4w-portal.ru/d4w/org-105/fsearch?practiceID=240'});
+    res.render('popup', { title: 'NDC RC' , d4w_url: 'https://d4w-portal.ru/d4w/org-105/fsearch?practiceID=240'});
   else
-    res.render('popup', { title: 'Express' , d4w_url: 'https://obtest18.centaurportal.com/d4w/org-16/fsearch'});
+    res.render('popup', { title: 'NDC RC' , d4w_url: 'https://obtest18.centaurportal.com/d4w/org-16/fsearch'});
 });
 
 module.exports = router;
